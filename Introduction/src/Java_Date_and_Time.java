931ebffb131181713f7fa4cdd120d407d2fad778
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-02-07 21:12:25 +0000

    Update

---
+++ Introduction/src/Java_Date_and_Time.java
@@ -0,0 +1,45 @@
+// Author: Nazmul Hassan
+// Submit: HackerRank
+// Date: 07/02/2021
+// Language: JAVA
+
+// Hacker Rank Solution From Here
+
+import java.io.*;
+import java.time.LocalDate;
+
+class Result {
+    public static String findDay(int month, int day, int year) {
+        int d = Integer.valueOf(day);
+        int m = Integer.valueOf(month);
+        int y = Integer.valueOf(year);
+        LocalDate date = LocalDate.of(y, m, d);
+        return date.getDayOfWeek().toString();
+
+    }
+}
+
+
+public class Java_Date_and_Time {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
--- Strings/src/Tag_Content_Extractor.java
+++ Strings/src/Tag_Content_Extractor.java
@@ -5,5 +5,31 @@
 
 // Hacker Rank Solution From Here
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Tag_Content_Extractor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int testCases = Integer.parseInt(input.nextLine());
+        while (testCases > 0) {
+            String line = input.nextLine();
+
+            //Write your code here
+            boolean matchFound = false;
+            Pattern r = Pattern.compile("<(.+)>([^<]+)</\\1>");
+            Matcher m = r.matcher(line);
+            while (m.find()) {
+                System.out.println(m.group(2));
+                matchFound = true;
+            }
+            if (!matchFound) {
+                System.out.println("None");
+            }
+
+            testCases--;
+        }
+    }
 }
