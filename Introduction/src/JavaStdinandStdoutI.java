ddcf2b14980f3688b30c0857fca7ec08e9cdcdf2
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-02-07 18:04:47 +0000

    Update

---
+++ Introduction/src/JavaStdinandStdoutI.java
@@ -0,0 +1,24 @@
+// Author: Nazmul Hassan
+// Submit: HackerRank
+// Date: 05/02/2021
+// Language: JAVA
+
+// Hacker Rank Solution From Here
+
+import java.util.Scanner;
+
+public class JavaStdinandStdoutI {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Taking input and store in int
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        // Printing Output
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
