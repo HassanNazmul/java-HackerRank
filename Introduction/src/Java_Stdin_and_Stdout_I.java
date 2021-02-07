1652e3fa0f35552000affa038981c057cbd2740b
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-02-07 19:16:27 +0000

    Update

---
+++ Introduction/src/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,24 @@
+// Author: Nazmul Hassan
+// Submit: HackerRank
+// Date: 07/02/2021
+// Language: JAVA
+
+// Hacker Rank Solution From Here
+
+import java.util.Scanner;
+
+public class Java_Stdin_and_Stdout_I {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Taking inputs in int
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        // Printing Output
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
---
+++ Introduction/src/Welcome_to_Java.java
@@ -0,0 +1,13 @@
+// Author: Nazmul Hassan
+// Submit: HackerRank
+// Date: 07/02/2021
+// Language: JAVA
+
+// Hacker Rank Solution From Here
+
+public class Welcome_to_Java {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
